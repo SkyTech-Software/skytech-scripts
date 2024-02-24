908446637c9c4727f20511c189821bdfbb3a8690
xnykram <nykram@wp.pl>
2024-02-24 16:14:17 +0100

    - fix router path

--- backend/api_v1/api.py
+++ backend/api_v1/api.py
@@ -4,4 +4,4 @@ from backend.api_v1.endpoints import imei_checker, mailer
 
 api_router = APIRouter()
 api_router.include_router(imei_checker.router, tags=["imei-checker"], prefix="/imei")
-api_router.include_router(mailer.router, tags=["mailer"], prefix="/send-email")
+api_router.include_router(mailer.router, tags=["mailer"], prefix="/mailer")
