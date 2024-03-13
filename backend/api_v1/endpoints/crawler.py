feb158988226f7f015604b778bc04c3726dc97a4
mateusz2173 <mateusz_burdyna@protonmail.com>
2024-03-13 22:15:51 +0100

    Added keyword endpoint impl

--- backend/api_v1/endpoints/crawler.py
+++ backend/api_v1/endpoints/crawler.py
@@ -1,6 +1,13 @@
 from fastapi import APIRouter
 from backend.schema.crawler import CrawlerInputLinks, CrawlerInputKeyword
-from backend.api_v1.commands.crawler_apk.apk_font_extractor import run_extractor
+from backend.api_v1.commands.crawler_apk.apk_font_extractor import (
+    run_extractor,
+    collect_links_by_author,
+)
+from backend.api_v1.commands.crawler_apk.messages import (
+    keyword_mail_response,
+    link_mail_response,
+)
 from backend.api_v1.commands.mailer.mailer import send_mail
 from backend.schema.mail import MailResponse
 
@@ -12,16 +19,23 @@ async def process_from_links(crawler_input: CrawlerInputLinks) -> MailResponse:
     zip_file = run_extractor(crawler_input.links)
     return send_mail(
         target_email=crawler_input.target_email,
-        subject="Fonts Extractor",
-        message="Please find attached .zip file.",
+        subject=link_mail_response(),
+        message="please find attached .zip file.",
         file_content=zip_file,
         file_content_type="zip",
-        file_name="Fonts.zip",
+        file_name="fonts.zip",
     )
 
 
-# PLACEHOLDER #
 @router.post("/process-from-keyword")
-async def process_from_keyword(crawler_input: CrawlerInputKeyword) -> int:
-    # TODO: logic keyword
-    return 200
+async def process_from_keyword(crawler_input: CrawlerInputKeyword) -> MailResponse:
+    links = collect_links_by_author(crawler_input.keyword)
+    zip_file = run_extractor(links)
+    return send_mail(
+        target_email=crawler_input.target_email,
+        subject="fonts extractor",
+        message=keyword_mail_response(crawler_input.keyword),
+        file_content=zip_file,
+        file_content_type="zip",
+        file_name="fonts.zip",
+    )
