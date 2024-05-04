c3a7618101117e9226f7ee88008fddda008e544b
mateusz2173 <mateusz_burdyna@protonmail.com>
2024-05-04 13:14:30 +0200

    Added more countries to scraper

--- wrappers/google-play-scraper-wrapper/src/index.js
+++ wrappers/google-play-scraper-wrapper/src/index.js
@@ -3,10 +3,18 @@ import gplay from "google-play-scraper";
 // Get first argument from command line
 let author = process.argv[2];
 
-gplay.developer({devId: author, num: 200, country: 'pl'}).then(output_data).catch(err => {});
+let set = new Set();
 
-function output_data(data) {
-  data.forEach((app) => {
-    console.log(app["appId"]);
-  });
+async function get_data() {
+  await gplay.developer({ devId: author, num: 200, country: 'pl' }).then(data => data.forEach(d => set.add(d["appId"]))).catch(e => { });
+  await gplay.developer({ devId: author, num: 200, country: 'lt' }).then(data => data.forEach(d => set.add(d["appId"]))).catch(e => { });
+  await gplay.developer({ devId: author, num: 200, country: 'lv' }).then(data => data.forEach(d => set.add(d["appId"]))).catch(e => { });
+  await gplay.developer({ devId: author, num: 200, country: 'ee' }).then(data => data.forEach(d => set.add(d["appId"]))).catch(e => { });
+  await gplay.developer({ devId: author, num: 200, country: 'cz' }).then(data => data.forEach(d => set.add(d["appId"]))).catch(e => { });
 }
+
+get_data().then(() => {
+  for (let app of set) {
+    console.log(app);
+  }
+});
